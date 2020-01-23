36295b997529812020db2385375775a5e68ab9ce
wparedes <wparedesgt@gmail.com>
2020-01-23 14:16:07 -0600

    imputando datos

--- seccion03/analisis/13_data_correction.R
+++ seccion03/analisis/13_data_correction.R
@@ -21,3 +21,27 @@ crime_data02 <- crime_data %>% gather(key = "crime_type",
 head(crime_data02)
 
 crime_data02
+
+crime_data03 <- crime_data %>% gather(key = "Crime Type", 
+                                      value = "Arrest_estimate", 
+                                      Murder, Assault)
+head(crime_data03)
+
+
+#Funcion Spread
+
+crime_data04 <- crime_data02 %>% spread(key = "crime_type", 
+                                        value = "arrest_estimate")
+
+crime_data04
+
+crime_data05 <- crime_data %>% unite(col = "Murder_Assault", 
+                                     Murder, Assault, 
+                                     sep = "_")
+head(crime_data05)
+
+crime_data06 <- crime_data05 %>% separate(col = "Murder_Assault", 
+                                          into = c("Murder", "Assault"), 
+                                          sep = "_")
+
+head(crime_data06)
---
+++ seccion03/analisis/14_imputing_data.R
@@ -0,0 +1,8 @@
+#14
+library(tidyverse)
+library(mice)
+
+housing_data <- read_csv("seccion03/data/housing-with-missing-value.csv")
+
+housing_data
+
