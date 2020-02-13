035596191e1118bb3947a931a056c4c3313f8a62
wparedes <wparedesgt@gmail.com>
2020-02-13 11:06:44 -0600

    inicio comparaciones

--- seccion04/analisis/06_lattice.R
+++ seccion04/analisis/06_lattice.R
@@ -9,7 +9,23 @@ auto$cylinders <-factor(auto$cylinders,
                         levels = c(3,4,5,6,8),
                         labels = c("3cil", "4cil", "5cil", "6cil", "8cil"))
 
-str(auto)
-cat(auto$mpg)
+#boxplot
 
-attach(auto)
+bwplot(~auto$mpg | auto$cylinders, 
+       main = "MPG segun Cilindros", 
+       xlab = "Millas por Galon", 
+       layout = c(2,3), 
+       aspect = 1)
+
+#scaterplot
+
+xyplot(mpg~weight | cylinders, data = auto,
+                main = "Peso vrs Consumo, Vrs Cilindros", 
+                xlab = "Peso", 
+                ylab = "Consumo (MPG)")
+
+##Densityplot, splom
+
+trellis.par.set(theme = col.whitebg())
+
+
