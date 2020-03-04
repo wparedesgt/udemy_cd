7c1b390f008205dc27139ee59cf9ee3a46bd7246
wparedes <wparedesgt@gmail.com>
2020-03-04 14:15:35 -0600

    ACP

--- seccion07/analisis/06_advanced_clustering.R
+++ seccion07/analisis/06_advanced_clustering.R
@@ -1,2 +1,32 @@
 #Clustering basados en densidades
 
+library(tidyverse)
+library(factoextra)
+library(fpc)
+library(mclust)
+
+data("multishapes")
+
+par(mfrow=c(1,1))
+
+dataPoints <- multishapes[,1:2]
+
+plot(dataPoints)
+
+
+km <- kmeans(dataPoints, 5)
+
+fviz_cluster(km, data = dataPoints)
+
+dsfit <- dbscan(dataPoints, eps = 0.15, MinPts = 5)
+
+dsfit
+
+fviz_cluster(dsfit, dataPoints, geom = "point")
+
+
+mclust <- Mclust(dataPoints)
+
+plot(mclust)
+
+summary(mclust)
---
+++ seccion07/analisis/07_acp.R
@@ -0,0 +1,1 @@
+#Analisis Componentes Principales 
